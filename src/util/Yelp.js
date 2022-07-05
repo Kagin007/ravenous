555c963cd5b3d56c926f2e53db51607fc5a060f6
Kagin007 <adamschulte148@hotmail.com>
2022-07-05 13:09:36 -0400

    Yelp API written and connected to searchabr component

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,5 +1,32 @@
-// require('dotenv').config()
+const apiKey = process.env.REACT_APP_YELP_API_KEY
 
-const apiKey = process.env.YELP_API_KEY
+const Yelp = {
+  async search(term, location, sortBy) {
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+      headers: {
+      Authorization: `Bearer ${apiKey}`
+      }
+    }).then(response => {
+      return response.json()
+    }).then(jsonResponse => {
+      if(jsonResponse.businesses) {
+        return jsonResponse.businesses.map(business => {
+          return {
+            id: business.id,
+            imageSrc: business.image_url,
+            name: business.name,
+            address: business.location.address1,
+            city: business.location.city,
+            state: business.location.state,
+            zipCode: business.location.zip_code,
+            category: business.categories[0].title,
+            rating: business.rating,
+            reviewCount: business.review_count
+          }
+        })
+      }
+    })
+  }
+};
 
-const Yelp = {};
+export default Yelp;
